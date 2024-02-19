3fe0977a44fad83e6156ee645c0ffdc9f8763f31
jim24Santome <jim@mobilenik.com>
2024-02-18 21:48:29 -0300

    Actualizamos version de material ya que contenia errores.

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -3,7 +3,7 @@ object Versions {
     /** General **/
 
     const val hilt = "2.45"
-    const val material = "1.11.0"
+    const val material = "1.12.0-alpha03"
     const val appCompat = "1.6.1"
     const val lifeCycle = "2.7.0"
     const val coroutines = "1.7.1"
